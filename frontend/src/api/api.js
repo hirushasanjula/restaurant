e06409094bb8ccdec408b9a5e05cb75213041c52
hirushasanjula <10898650@students.plymouth.ac.uk>
2025-04-17 12:29:27 +0530

    update

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const baseURL = process.env.NODE_ENV === 'production' 
-  ? 'https://restaurant-backend-yr2k.onrender.com'
+  ? 'https://restaurant-backend-yr2k.onrender.com/api'
   : '/api';
 
 const api = axios.create({
